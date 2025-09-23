bf8d7f18641f91ac86e7ff3e124e9bca357b70f8
Itswali <luckywali0986@gmail.com>
2025-09-23 21:22:36 +0500

    introduce book management

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import { connectDB } from "./database/db.js";
 import { errorMiddleware } from "./middlewares/errorMiddlewares.js";
 import authRouter from "./routes/authRouter.js";
-import router from "./routes/authRouter.js";
+import bookRouter from "./routes/bookRouter.js";
 
 export const app = express ();
 
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}))
 
 app.use("/api/v1/auth", authRouter);
-http://localhost:4000/api/v1/auth/register
+app.use("/api/v1/book", bookRouter);
 
 connectDB();
 
