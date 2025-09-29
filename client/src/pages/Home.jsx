5d0d5537b35f668c78806d58307ccd0fe8a21178
Itswali <luckywali0986@gmail.com>
2025-09-29 15:58:24 +0500

    create slices now on home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import {useSelector} from "react-redux";
+import {Navigate} from "react-router-dom";
+import SideBar from "../layout/SideBar";
 
 const Home = () => {
-  return <></>;
+  const [isSideBarOpen,  setIsSideBarOpen] = useState(false);
+  const [selectedComponent, setSelectedComponent] = useState("");
+
+  const {user, isAuthenticated} = useSelector(state => state.auth);
+
+  if(!isAuthenticated) {
+    return <Navigate to={"/login"}/>;
+  }
+
+  return <>
+  <div>
+    <div>
+      <GiHamburgerMenu />
+    </div>
+    <SideBar />
+  </div>
+  </>;
 };
 
 export default Home;
